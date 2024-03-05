0ee537ac2c93611dd9524d93ac22607f1292fe0c
Mehdi Abbadi <mabbadi@fu-r10-p4.s19.be>
2024-03-05 11:31:07 +0100

    env test

--- test.c
+++ test.c
@@ -6,9 +6,29 @@
 /*   By: mabbadi <mabbadi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 12:06:55 by mabbadi           #+#    #+#             */
-/*   Updated: 2024/03/01 12:07:48 by mabbadi          ###   ########.fr       */
+/*   Updated: 2024/03/05 11:30:48 by mabbadi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "minishell"
+// #include "minishell.h"
+
+// int	main(int argc, char **argv, char **envp)
+// {
+//     int i = 0;
+//     int j = 0;
+    
+//     char **myenv;
+//     myenv = malloc(sizeof(envp));
+//     while (envp[i])
+//     {
+//         // myenv[i] = malloc(sizeof(env[i]));
+//         myenv[i] = envp[i];
+//         printf("%d : %s\n",i, myenv[i]);
+//         // printf("%d : %s\n",i, env[i]);
+//         i++;
+//     }
+//     printf("\n%s\n", getenv("PATH"));
+//     return 0;
+// }
+
 
